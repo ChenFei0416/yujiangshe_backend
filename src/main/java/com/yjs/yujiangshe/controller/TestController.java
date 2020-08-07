9480f3ae74c128961c07562dccfb826df64ad460
HibernateChen <44768084+HibernateChen@users.noreply.github.com>
2020-08-07 10:37:17 +0800

    Create TestController.java

---
+++ src/main/java/com/yjs/yujiangshe/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.yjs.yujiangshe.controller;
+
+/**
+ * @Desc
+ * @Author ChenFei
+ * @Date 2020/8/7 10:31
+ */
+public class TestController {
+    public void test(){
+        System.out.println(".............");
+    }
+}
